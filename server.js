5150c4ef6eb64ecf96b222c9e6d7aef5166e55e5
Jesus E Kahwati A <jekahwati@falabella.cl>
2024-05-14 04:06:42 -0400

    dsd

--- server.js
+++ server.js
@@ -38,5 +38,6 @@ const PORT = process.env.PORT || config.PORT;
 server.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
     console.log(`Path: ${PREFIX}`);
+    console.log(process.env);
 });
 
