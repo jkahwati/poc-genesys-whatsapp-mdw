ed13377571f19ce358ad17cf732984e8d91c6a77
Jesus E Kahwati A <jekahwati@falabella.cl>
2024-05-14 14:33:02 -0400

    removing extra line

--- server.js
+++ server.js
@@ -38,6 +38,5 @@ const PORT = process.env.PORT || config.PORT;
 server.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
     console.log(`Path: ${PREFIX}`);
-    console.log(process.env);
 });
 
